d48c61360d2459e3bd66ff11423bd45d81c29fdc
kelvinobuvie <kelvinobuvie@gmil.com>
2023-09-13 17:10:05 +0100

    feat installations

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-amber-400">
+      <p className="text-red-700">Hello world</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,5 @@
+import "header.css" from /Headers.css
+import tailwindConfig from "../tailwind.config"
+<div>
+<div className="nav-bar"></div>
+</div>
