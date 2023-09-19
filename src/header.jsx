35a83462a1f459ecbe24dc50590a0a8ab2972098
kelvin obuvie <kelvinobuvie@gmail.com>
2023-09-19 06:09:00 +0100

    css changes

--- src/header.jsx
+++ src/header.jsx
@@ -1,39 +1,57 @@
-import logo from "./assets/tv.png"
-import elipse from "./assets/Ellipse 1.svg"
-import menu from "./assets/Menu alt 4.svg"
-import poster from "./assets/Poster.png"
-import play from "./assets/play.svg"
-const Header = ()=> {
-    return(
-        <>
-        {/* <img src={poster} alt="" className="poster"/> */}
-        <header className="text-white flex">
-            <div className="flex flex-1 justify-between">
-            <a href="#" className="logo flex gap-3 items-center"><img src={logo} alt="" />Movie Boxss</a>
-            <input type="text" placeholder="search movie" id="" />
-            <ul className="nav-bar flex items-center gap-3">
-                <li></li>
-                <li className="sign-in">sign In</li>
-                <li className="menu icon flex"><img src={elipse} alt=""  /></li>
-            </ul>
-            </div>            
-        </header>
-        <div>
-            <section className="home text-white">
-                <div className="home-text w-full max-w-xs leading-5">
-                    <span>John Wick 3 : Parabellum</span>
-                    <p className="rating"><img src="" alt="" />86.0 / 100 <img src="" alt="" />97%</p>
-                    <p>John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.</p>
-                    {/* <a href="#" class="btn">Watch Trailer</a> */}
-                    <button className="btn"><img src={play} alt="" />Watch Trailer</button>
-                </div>
-            </section>
-        </div>
-        
-        
-        
-        </>
-    )
-}
+import logo from "./assets/tv.png";
+import elipse from "./assets/Ellipse 1.svg";
+import menu from "./assets/Menu alt 4.svg";
+import poster from "./assets/Poster.png";
+import play from "./assets/play.svg";
+const Header = () => {
+	return (
+		<>
+			{/* <img src={poster} alt="" className="poster"/> */}
+			<header className="text-white flex">
+				<div className="flex flex-1 justify-between">
+					<a href="#" className="logo flex gap-3 items-center">
+						<img src={logo} alt="" />
+						Movie Boxss
+					</a>
+					<input
+						type="text"
+						placeholder="search movie"
+						id=""
+						className="input"
+					/>
+					<ul className="nav-bar flex items-center gap-3">
+						<li></li>
+						<li className="sign-in">sign In</li>
+						<li className="menu icon flex">
+							<img src={elipse} alt="" />
+						</li>
+					</ul>
+				</div>
+			</header>
+			<div>
+				<section className="home text-white">
+					<div className="home-text w-full max-w-xs leading-4 gap-1">
+						<span>John Wick 3 : Parabellum</span>
+						<p className="rating flex">
+							<img src="" alt="" />
+							86.0 / 100 <img src="" alt="" />
+							97%
+						</p>
+						<p className="gap-1">
+							John Wick is on the run after killing a member of the
+							international assassins' guild, and with a $14 million price tag
+							on his head, he is the target of hit men and women everywhere.
+						</p>
+						{/* <a href="#" class="btn">Watch Trailer</a> */}
+						<button className="btn">
+							<img src={play} alt="" />
+							Watch Trailer
+						</button>
+					</div>
+				</section>
+			</div>
+		</>
+	);
+};
 
 export default Header;
